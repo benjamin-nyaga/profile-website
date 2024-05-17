dee6def2e3eabad3ce02a2bde40704fe39d7f58c
benjamin-nyaga <149682524+benjamin-nyaga@users.noreply.github.com>
2024-05-17 19:59:37 +0300

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+/*alert('hello world')
+console.log('hello world')
+let email = ('muthami@gmail.com')
+console.log(email);
+let firstname = ('benjamin')
+let secondname = ('nyaga')
+let fullname = firstname + '  ' + secondname;
+console.log(fullname);
+console.log(fullname[4])
+console.log(fullname.length)
